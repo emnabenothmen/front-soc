cd7a52534f9e1979ad19216f2034045cc2826f0c
emnabenothmen <emnabenothmen91@gmail.com>
2023-12-07 12:01:51 +0100

    Premier commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {path : 'login' , component: LoginComponent},
+
+   {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+        {
+      path: '',
+      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
+  }]},
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss','./assets/css/style.css','./assets/css/font-awesome.min.css','./assets/css/bootstrap.min.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
